ee578cc24ad3c2a9b62f4a24a938a90a9e85d736
mdyasinahmed <yasinahmed7875@gmail.com>
2024-07-12 11:44:40 +0600

    add welcome screen and login page image

--- uniKernel/lib/login_page.dart
+++ uniKernel/lib/login_page.dart
@@ -1,75 +1,181 @@
 import 'package:flutter/material.dart';
 import 'sign_up_page.dart';
-import 'university_id_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue, Colors.lightBlueAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'UCOPILOT',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                ),
-                SizedBox(height: 40),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    filled: true,
-                    fillColor: Colors.white,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 80),
+              SizedBox(
+                width: 180,
+                height: 180,
+                child: Image.asset('assets/logo.png'),
+              ),
+              SizedBox(height: 40),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  hintText: 'example@gmail.com',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Color(0xFF4E60FF)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Color(0xFF4E60FF)),
                   ),
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
+                style: TextStyle(color: Color(0xFF4E60FF)),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Color(0xFF4E60FF)),
                   ),
-                  obscureText: true,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Color(0xFF4E60FF)),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
+                style: TextStyle(color: Color(0xFF4E60FF)),
+              ),
+              SizedBox(height: 40),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFF4E60FF), Color(0xFF02BDFF)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => UniversityIdPage()),
-                    );
+                    // Handle login
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
                   },
-                  child: Text('Login'),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.blue, backgroundColor: Colors.white,
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpPage()),
-                    );
-                  },
-                  child: Text('Sign Up', style: TextStyle(color: Colors.white)),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('Login with Social', style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'or',
+                style: TextStyle(color: Color(0xADADADFF)),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Login with',
+                style: TextStyle(color: Color(0xFF4E60FF)),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.account_circle, color: Color(0xFF4E60FF)),
+                    iconSize: 40,
+                    onPressed: () {
+                      // Handle Google login
+                    },
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                    icon: Icon(Icons.apple, color: Color(0xFF4E60FF)),
+                    iconSize: 40,
+                    onPressed: () {
+                      // Handle Apple login
+                    },
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                    icon: Icon(Icons.facebook, color: Color(0xFF4E60FF)),
+                    iconSize: 40,
+                    onPressed: () {
+                      // Handle Facebook login
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(color: Color(0xFF4E60FF)),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Color(0xFF4E60FF),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+            ],
           ),
         ),
       ),
